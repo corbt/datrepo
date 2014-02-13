1814f8c54544ec0b0c527905daf0fb0ce1c17654
Kyle Corbitt <kyle@corbt.com>
2014-02-12 22:40:07 -0700

    squash i18n warning

--- site/config/application.rb
+++ site/config/application.rb
@@ -19,6 +19,7 @@ module Datrepo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.config.enforce_available_locales = true
 
     config.generators do |g|
       g.test_framework :rspec, fixture: true
