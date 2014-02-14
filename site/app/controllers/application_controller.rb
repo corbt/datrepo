c560d61f553fa06afefdfb2d59b2b719782dd022
Kyle Corbitt <kyle@corbt.com>
2014-02-13 21:25:47 -0700

    login redirects to previous page

--- site/app/controllers/application_controller.rb
+++ site/app/controllers/application_controller.rb
@@ -5,6 +5,27 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  after_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        request.fullpath != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    session[:previous_url] || user_path(current_user.username)
+  end
+
   protected
 
   def configure_permitted_parameters
