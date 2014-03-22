20a817ad9e163e395b34e8e75e26ed85fe29867e
Kyle Corbitt <kyle@corbt.com>
2014-03-22 16:35:30 -0600

    reorganize directory structure for capistrano

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Datrepo::Application.config.secret_key_base = '41c71945111cc324ccabd4bacdd7f257b67110186538410e52cc265fee24f3034ec8a35732b09ec3ec117686eed3d19f8e0835b403c549260e71ace6627013ebecaf79ec60ed381bf69fdc32e59820477206d522a4f0d8711fac310c6792c9896627'
